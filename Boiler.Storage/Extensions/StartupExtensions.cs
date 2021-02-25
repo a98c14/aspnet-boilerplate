8383605e04e5dccee6c90921033a955707d309b2
selim <selimyesilkaya@gmail.com>
2021-02-25 23:13:52 +0300

    add azure blob storage service for boiler.storage

---
+++ Boiler.Storage/Extensions/StartupExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Boiler.Storage.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Boiler.Storage.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static void AddStorageServices(this IServiceCollection services)
+        {
+            services.AddScoped<BlobStorageService>();
+        }
+    }
+}
