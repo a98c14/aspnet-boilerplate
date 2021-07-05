34723ff8463f1740a229c2374c7e23a11fcafb86
Yavuz Selim Gügen <ygugen@windowslive.com>
2021-07-05 14:54:45 +0300

    Create DatabaseSeedAgent.cs

---
+++ Boiler.Api/Extensions/DatabaseSeedAgent.cs
@@ -0,0 +1,45 @@
+﻿using Boiler.Api.Features.Auth;
+using Boiler.Api.Features.Auth.Request;
+using Boiler.Domain.Auth;
+using Boiler.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boiler.Api.Extensions
+{
+    public class DatabaseSeedAgent
+    {
+        private readonly IAuthService m_AuthService;
+        private readonly DataContext m_db;
+
+        public DatabaseSeedAgent(IAuthService authService, DataContext db)
+        {
+            m_AuthService = authService;
+            m_db = db;
+        }
+
+        public async Task SeedDatabase()
+        {
+            if (!m_db.Accounts.Any(a => a.Role == Role.SuperAdmin))
+            {
+                var superAdminResponse = m_AuthService.Register(
+                    new RegisterRequest
+                    {
+                        Email = "super@admin.com",
+                        Password = "123456"
+                    },
+                    Role.SuperAdmin);
+                var adminResponse = m_AuthService.Register(
+                    new RegisterRequest
+                    {
+                        Email = "admin@ecma.com",
+                        Password = "123456",
+                    },
+                    Role.Admin
+                    );
+            }
+        }
+    }
+}
